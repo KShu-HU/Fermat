d5259ecdcaff68707eba23880f14b8027ac434a1
Katabami <katabami.syu@gmail.com>
2025-07-03 16:02:43 +0900

    temp_theo_del

---
+++ Project/Mathlib/Example.lean
@@ -0,0 +1,38 @@
+--min_import
+import Mathlib.FieldTheory.Finite.Basic
+import Mathlib.Algebra.Field.ZMod
+import Mathlib.Data.ZMod.Units
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Fintype.BigOperators
+import Mathlib.Data.Nat.Choose.Basic
+import Mathlib.Tactic
+import Mathlib.Data.Nat.ModEq
+import Mathlib.Data.Nat.Prime.Basic
+
+open scoped BigOperators
+
+-- theorem1
+theorem katabami_theorem_fermat1 {p : ℕ} (hp : p.Prime) (ha : a.Coprime p) :
+    a ^ (p - 1) ≡ 1 [MOD p] := by sorry
+
+--theorem2 二項定理
+theorem katabami_theorem_fermat2 {p : ℕ} (hp : p.Prime) (ha : a.Coprime p) :
+    a ^ (p - 1) ≡ 1 [MOD p] := by sorry
+
+--theorem3 剰余群
+theorem card (n : ℕ) [Fintype (ZMod n)] : Fintype.card (ZMod n) = n := by
+  cases n with
+  | zero => exact (not_finite (ZMod 0)).elim
+  | succ n => convert Fintype.card_fin (n + 1) using 2
+  --
+
+theorem card_units (p : ℕ) [Fact p.Prime] : Fintype.card (ZMod p)ˣ = p - 1 := by
+  rw [Fintype.card_units, card]
+  --card_unitsはZmodP(=ℤ/pℤ)の単元はp-1個のこと。
+
+theorem katabami_theorem_fermat3 (p : ℕ) [Fact p.Prime] (a : (ZMod p)ˣ) : a ^ (p - 1) = 1 := by
+  rw [← card_units p, pow_card_eq_one]
+  --card_unitsは
+  --pow_card_eq_oneはラグランジュの定理の帰結(Gの任意の元gに対して、gの位数はGの位数の約数なのでg^|G|=1)
+
+#min_imports
